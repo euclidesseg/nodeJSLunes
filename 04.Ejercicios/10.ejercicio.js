9380f764bea40b9ea62206e0cbf6610eb3ed0775
euclidesseg <euder0206@gmail.com>
2023-03-07 01:50:40 -0500

    se resuelve ejercicio numero 10

--- 04.Ejercicios/10.ejercicio.js
+++ 04.Ejercicios/10.ejercicio.js
@@ -1,12 +1,65 @@
 
+let alimentos = [
+    "Brócoli",
+    "Zanahoria",
+    "Cebolla",
+    "Espinaca",
+    "Acelga",
+    "Alcachofa",
+    "Chayote",
+    "Calabaza",
+    "Calabacín",
+    "Hormigas",
+    "Grillos",
+    "Chapulines",
+    "Caracoles",
+    "Cangrejo",
+    "Pulpo",
+    "Pato",
+    "Cordero",
+    "Conejo",
+    "Ternera",
+    "Tilapia"
+];
 
-function generarObgeto(){
-    for(let i = 1; i < 301; i++ ){
-        let nombre;
-        let tipo;
+tipos = ["vegetal", "Animal", "Insecto"]
+let alimentosCompletos = []
+function generarObgeto() {
+    for (let i = 1; i <= 20; i++) {
+
+        let nombre = alimentos[Math.floor(Math.random() * alimentos.length)]
+        let tipo = tipos[Math.floor(Math.random() * tipos.length)]
         let nivelEnergia = Math.round(Math.random(1) * (100, 500))
-        console.log(nivelEnergia)
+
+        alimentosCompletos.push({
+            nombre: nombre,
+            tipo: tipo,
+            nivelEnergia: nivelEnergia
+        })
+
     }
-   
+
+    function obtenerVegetales(tipoAlimento, callBack) {
+        setTimeout( () => {
+            let alimentosVegetales = alimentosCompletos.filter( (alimento) => {
+                if(alimento.tipo === "vegetal" ){
+                    if(alimento.nivelEnergia > 200){
+                        return alimento
+                    }
+                }
+
+            })
+            console.log(alimentosVegetales)
+            callBack(alimentosVegetales)
+        }, 5000)
+        
+    }
+    obtenerVegetales(null, function(alimentos){
+        let suma = 0
+        alimentos.forEach((alimento) => {
+            suma = suma + alimento.nivelEnergia
+        })
+        console.log(suma);
+    })
 }
 generarObgeto()
